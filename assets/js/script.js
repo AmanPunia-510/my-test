89197c3fa2843df72890c14d31a6cb553595666a
AmanPunia-510 <puniaaman888@gmail.com>
2024-06-04 13:06:16 +0530

    project almost complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,35 @@ menu.addEventListener("click", () => {
     contain.style.position = "unset";
   }
 });
+
+// accrodian
+
+let accordian_items = document.querySelectorAll(".accordian-box");
+
+accordian_items.forEach((items) => {
+  let paragraph = items.querySelector(".accordian-p");
+  let heading = items.querySelector(".accordian-txt-or-svg");
+  let svg = items.querySelector(".accordian-svg");
+
+  heading.style.cursor = "pointer";
+  heading.addEventListener("click", () => {
+    accordian_items.forEach((other) => {
+      if (other !== items) {
+        let paragraph = other.querySelector(".accordian-p");
+        let svg = other.querySelector(".accordian-svg");
+        paragraph.classList.add("d-none");
+        svg.style.transform = "rotate(180deg)";
+      }
+    });
+
+    if (paragraph.classList.contains("d-none")) {
+      paragraph.classList.remove("d-none");
+      paragraph.classList.add("d-block");
+      svg.style.transform = "rotate(0deg)";
+    } else {
+      paragraph.classList.remove("d-block");
+      paragraph.classList.add("d-none");
+      svg.style.transform = "rotate(180deg)";
+    }
+  });
+});
